7b9c9f3c5765af3812977d279933425e6f0902d8
kmc629 <kmcboss7@gmail.com>
2023-06-04 23:45:09 +0900

    springboot practice

---
+++ src/main/java/me/kimmincheon/springbootdeveloper/MemberRepository.java
@@ -0,0 +1,8 @@
+package me.kimmincheon.springbootdeveloper;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
